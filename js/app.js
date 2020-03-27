7a488f195d4b2b29c9b502ea02e057546add85ec
brian5021 <brian05-21@hotmail.com>
2020-03-26 21:40:38 -0500

    consumiendo servicio

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
           }
         });
 
+
 //efecto de scroll suave
     $(document).ready(function() {
             $('a[href^="#"]').click(function() {
@@ -21,3 +22,35 @@
               return false;
             });
     });
+
+
+    //Comienza Vanilla JS
+
+    var noticias = document.getElementById("nosotrosContenido");
+
+    fetch('js/servicio.json')
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data.noticias);
+
+        datano = data.noticias;
+
+        cards(datano);
+      });
+
+       function cards(datano){
+        noticias.innerHTML = ``;
+
+         for(let valor of datano){
+
+          noticias.innerHTML += `<div class="card col-md-4 col-sm-12" style="width: 23rem;">
+          <img src="${valor.imagen}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${valor.titulo}</h5>
+            <p class="card-text">${valor.descripcion}.</p>
+            <a href="${valor.link}" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>`;
+
+        }
+      }
